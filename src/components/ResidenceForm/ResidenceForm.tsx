7efa21455f75afe1919583ccef02adb04f0a0e52
Eddie Dover <ed@eddiedover.dev>
2022-12-14 07:53:55 -0800

    indexes are fun

--- src/components/ResidenceForm/ResidenceForm.tsx
+++ src/components/ResidenceForm/ResidenceForm.tsx
@@ -4,16 +4,14 @@ import ResidenceFormMetaData from './types';
 
 export const ResidenceForm = () => {
     const [resForms, setResForms] = useState<ResidenceFormMetaData[]>([]);
-    let currentSequence = 0
     return (
         <div>
-            {resForms.map((resForm, currentSequence) => {
-                return <ResidenceFormPart form={resForm} sequence={currentSequence}/>;
+            {resForms.map((resForm, index) => {
+                return <ResidenceFormPart form={resForm} sequence={index}/>;
             })}
             <button type="button" onClick={() => {
                 const newForm = new ResidenceFormMetaData();
                 setResForms([...resForms, newForm]);
-                currentSequence = currentSequence + 1;
             }}>Add</button>
             { resForms.length > 0 ? (<button type="button" onClick={ () => {
                 console.log(resForms);
